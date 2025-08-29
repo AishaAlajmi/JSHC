c2b58ffb50e8ae48edb5aabdb17d4625c5f27c4e
AishaAlajmi <105488603+AishaAlajmi@users.noreply.github.com>
2025-08-30 02:22:40 +0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-// =============================================
-// filepath: src/main.jsx
-// =============================================
-import React from "react";
-import { createRoot } from "react-dom/client";
-import HPVDemo from "./HPVDemo.jsx";
-
-
-createRoot(document.getElementById("root")).render(<HPVDemo />);
